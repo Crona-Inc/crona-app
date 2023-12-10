e1fe7f7e4bb8dd7ccce7f773978ecee29dba3fc8
Eugene Kvasnyi <eugene.kwasny@gmail.com>
2023-12-10 13:16:47 +0200

    Add TimeLog entity

---
+++ src/Entity/TimeLog.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TimeLogRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TimeLogRepository::class)]
+class TimeLog
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $start = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $finish = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $duration = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStart(): ?\DateTimeImmutable
+    {
+        return $this->start;
+    }
+
+    public function setStart(\DateTimeImmutable $start): static
+    {
+        $this->start = $start;
+
+        return $this;
+    }
+
+    public function getFinish(): ?\DateTimeImmutable
+    {
+        return $this->finish;
+    }
+
+    public function setFinish(?\DateTimeImmutable $finish): static
+    {
+        $this->finish = $finish;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(?int $duration): static
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+}
